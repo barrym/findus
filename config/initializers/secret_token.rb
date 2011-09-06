c7c7d1f3c0d4abb0a1407452720119211cb12e70
Barry Mitchelson <barry.mitchelson@gmail.com>
2011-09-06 20:20:54 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Findus::Application.config.secret_token = 'c3469a7c71848f9a2e36a12535eac4a033c86d482458e4764f13edad99bad93b4f6161d31344533403d5b18935986f3365e2ff9822b3a251e766bda5888a0277'
