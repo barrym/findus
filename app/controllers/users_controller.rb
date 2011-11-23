fd3c954fd30ced3863ae2293f6490f361473ac23
Barry Mitchelson <barry.mitchelson@gmail.com>
2011-11-23 18:47:03 +0000

    Added users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+
+  # before_filter :authenticate_user!
+
+  def show
+    user = User.find(params[:id])
+    render :json => user
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Findus::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :users
+
   # Sample resource route with options:
   #   resources :products do
   #     member do
