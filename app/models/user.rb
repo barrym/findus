4a566950a1bc1ab528d29f750d7553bd99337997
Barry Mitchelson <barry.mitchelson@gmail.com>
2011-11-24 20:38:40 +0000

    Added gravatar to top bar.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,17 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :username, :case_sensitive => false, :allow_blank => false
   validates_presence_of :username, :email
 
+  def gravatar_url(options = {})
+    options[:rating] ||= nil
+    options[:size] ||= nil
+    options[:default] ||= nil
+
+    grav_url = 'http://www.gravatar.com/avatar.php?'
+    grav_url << "gravatar_id=#{Digest::MD5.new.update(self.email)}"
+    grav_url << "&rating=#{options[:rating]}" if options[:rating]
+    grav_url << "&size=#{options[:size]}" if options[:size]
+    grav_url << "&default=#{options[:default]}" if options[:default]
+    grav_url
+  end
+
 end
